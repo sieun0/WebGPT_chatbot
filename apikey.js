29641d2c1583dbcb2829d6610b7195cd1703b5d5
sieun0 <85726398+sieun0@users.noreply.github.com>
2023-06-11 15:52:57 +0900

    first upload

---
+++ apikey.js
@@ -0,0 +1,3 @@
+const config = {
+    apikey: "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"
+}
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, send_from_directory
+from flask import request, jsonify
+from flask_cors import CORS
+import os
+import openai
+
+app = Flask(__name__)
+CORS(app)
+
+#API_KEY= os.getenv("FLASK_API_KEY")
+
+
+'''def chatGPT():
+    # set api key
+    openai.api_key = API_KEY 
+
+    # Call the chat GPT API
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+         {"role": "user", "content": f"한국의 대통령은 누구입니까?"},
+        ],
+        temperature=0,
+        max_tokens=2048
+    )
+    return completion
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True, port=5000)'''
+@app.route("/")
+def chat():
+    return send_from_directory('', 'chat.html')
+
+'''@app.route("/apikey.js")
+def get_apikey():
+    return send_from_directory('WebGPTproject_capstone', 'apikey.js')'''
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', debug=True, port=5000)
